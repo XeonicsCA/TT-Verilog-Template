0f2e15dc8a6d1abc517853822a2b4525beedef96
Xeonics <44719213+XeonicsCA@users.noreply.github.com>
2025-10-01 15:04:37 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,32 +15,52 @@ module tt_um_8_prog_counter (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  // sync ui_in controls to clk
+  logic [2:0] ctrl_q;
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) ctrl_q <= '0;
+    else ctrl_q <= ui_in[2:0];
+  end
 
-  // control loading/high-z using uio_in as inputs
-  wire en = uio_in[0];
-  wire load = uio_in[1];
-  wire oe = uio_in[2];
-  assign uio_oe = 8'h00;  // set uio to input
+  wire en = ctrl_q[0];
+  wire load = ctrl_q[1];
+  wire oe ctrl_1[2];
 
-  // variable to hold counter val
-  logic [7:0] count;
+  // detect one cycle pulse of load
+  logic load_q;
+  wire load_pulse = load & ~load_q;
+
+  always @(posedge clk, or negedge rst_n) begin
+    if (!rst_n) load_q <= 1'b0;
+    else load_q <= load;
+  end
+
+  // 3 state FSM sequence, 0=DRIVE, 1=RELEASE, 2=CAPTURE, then back to 0
+  logic [1:0] seq;  // 0,1,2
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) seq <= 2'd0;
+    else if (load_pulse) seq <= 2'd1;
+    else if (seq != 2'd0) seq <= (seq == 2'd2) ? 2'd0 : seq + 2'd1; // if in seq 2, bring back to seq 0. otherwise increment 1 to 2
+  end
 
   // counter
+  logic [7:0] count;
+
   always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-      count <= 8'h00;           // reset to 0
-    end else if (load) begin
-      count <= ui_in;          // load in value on rising edge
-    end else if (en) begin
-      count <= count + 8'h01;   // increment count by 1 bit
-    end
+    if (!rst_n) count <= 8'h00;            // reset counter
+    else if (seq == 2'd2) count <= uio_in; // if in capture state, read in uio_in
+    else if (en) count <= count + 8'h01;   // increment counter
   end
 
   // output
-  assign uo_out = oe ? count : 8'bz;      // high-z output if oe not enabled
-  assign uio_out = 0;   // all output pins must be assigned
+  assign uo_out = count;
+  assign uio_out = count;
+  // drive output only when in drive state and output is enabled
+  wire driving = (seq == 2'd0) & oe;
+  assign uio_oe = {8{driving}};
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, uio_in[7:3], 1'b0};     // concat and takes bitwise & (last bit set to 0, so always 0)
+  wire _unused = &{ena, ui_in[7:3], 1'b0};     // concat and takes bitwise & (last bit set to 0, so always 0)
 
 endmodule
