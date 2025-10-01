0a6b14edf555049d8f75d668a4c8eafb1395a3c0
Xeonics <44719213+XeonicsCA@users.noreply.github.com>
2025-09-30 20:26:56 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,26 +5,40 @@
 
 `default_nettype none
 
-module tt_um_8_prog_counter (clk, rst_n, en, load, load_val, out_en, count, bus);
-  parameter WIDTH = 8;
+module tt_um_8_prog_counter (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
-  input   clk, reset_n, en, load, output_en;
-  input   [WIDTH-1:0] load_val;
-  output  [WIDTH-1:0] count;
-  inout   [WIDTH-1:0] bus;
-  
+  // control loading/high-z using uio_in as inputs
+  wire en = uio_in[0]
+  wire load = uio_in[1];
+  wire oe = uio[2];
+  assign uio_oe = 8'h00;  // set uio to input
+
+  // variable to hold counter val
+  logic [7:0] count;
+
+  // counter
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      count <= '0;        // reset
+      count <= 8'h00;           // reset to 0
     end else if (load) begin
-      count <= load_val;      // load in value on rising edge
+      count <= uio_in;          // load in value on rising edge
     end else if (en) begin
-      count <= count + 1'b1;    // increment count by 1 bit
+      count <= count + 8'h01;   // increment count by 1 bit
   end
 
-  // tristate output driver
-  assign bus = out_en ? count : 'z;
+  // output
+  assign uo_out = oe ? count : 1'bz      // high-z output if oe not enabled
 
   // List all unused inputs to prevent warnings
+  wire _unused = &{ena, uio_in[7:3], 1'b0};     // concat and takes bitwise & (last bit set to 0, so always 0)
 
-end module
+endmodule
