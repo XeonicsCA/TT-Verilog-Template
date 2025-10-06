4f0133a7dc9d22ec9506daa15cc82cb64088e528
Xeonics <44719213+XeonicsCA@users.noreply.github.com>
2025-10-06 14:35:58 -0400

    updated project.v and test.py, reset makefile and requirements.txt

--- src/project.v
+++ src/project.v
@@ -17,19 +17,19 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // counter
-  // logic [7:0] count;
-  // always @(posedge clk or negedge rst_n) begin
-  //   if (!rst_n) count <= 8'h00;            // reset counter
-  //   else count <= count + 8'h01;   // increment counter
-  // end
-  // assign uo_out = count;
+  logic [7:0] count;
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) count <= 8'h00;            // reset counter
+    else count <= count + 8'h01;   // increment counter
+  end
+  assign uo_out = count;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in, 1'b0};
 
 endmodule
